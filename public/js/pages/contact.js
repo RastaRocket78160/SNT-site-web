93888c9352f1bb8f0cef9774f82f8f13a1b5fa26
Alex Animate Mp4 <48261899+AlexAnimateMP4@users.noreply.github.com>
2021-05-27 11:57:37 +0200

    Delete public directory

--- public/js/pages/contact.js
+++
@@ -1,3 +0,0 @@
-const clipboard = color => navigator.clipboard.writeText(color);
-const popoverTriggerList = [].slice.call(document.querySelectorAll(`[data-bs-toggle="popover"]`));
-const popoverList = popoverTriggerList.map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
